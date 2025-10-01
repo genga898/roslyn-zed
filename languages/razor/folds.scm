c05c8118c18494456dbf236b23a5133f6a007cfe
genga <genga898@gmail.com>
2025-10-01 20:07:45 +0300

    feat: initial commit

---
+++ languages/razor/folds.scm
@@ -0,0 +1,18 @@
+[
+  (razor_if)
+  (razor_switch)
+  (razor_for)
+  (razor_foreach)
+  (razor_while)
+  (razor_do_while)
+  (razor_try)
+  (razor_catch)
+  (razor_finally)
+  (razor_compound_using)
+  (razor_section)
+  (razor_block)
+  (class_declaration)
+  (method_declaration) 
+  (namespace_declaration)
+  (preproc_region)
+] @fold
